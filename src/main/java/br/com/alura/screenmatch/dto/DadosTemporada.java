d0db49bcf6ea98e85ba23142af5edc635c34e479
Anderson Ernesto <ernesto.franzee@gmail.com>
2024-11-22 11:18:23 -0300

    Projeto Inicial

---
+++ src/main/java/br/com/alura/screenmatch/dto/DadosTemporada.java
@@ -0,0 +1,6 @@
+package br.com.alura.screenmatch.dto;
+
+public record DadosTemporada(
+
+) {
+}
---
+++ src/main/java/br/com/alura/screenmatch/service/ApiKey.java
@@ -0,0 +1,6 @@
+package br.com.alura.screenmatch.service;
+
+public class ApiKey {
+   // ApiKey apiKey = new ApiKey(System.getenv("API_KEY"));
+
+}
